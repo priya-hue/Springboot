736d3f9d64b03a6487d978cb379a318760ecf1b5
priya-hue <60704045+priya-hue@users.noreply.github.com>
2024-12-02 16:10:59 +0530

    Add files via upload

---
+++ Dev.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev
+{
+    public void build()
+    {
+        System.out.println("Working on Spring boot project");
+        }
+}
